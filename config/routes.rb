03b2be43ec2f9d986425aef698a2fc6046f70e89
kOyou <kingrulen@gmail.com>
2017-02-26 20:14:01 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     member do
       post 'upvote'
       post 'lowvote'
+      get 'about'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
